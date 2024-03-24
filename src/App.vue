8bb38012f542a38a9394ebb7a50af3999fe87a9c
Dominic Hiland <dominic.dev.email@gmail.com>
2024-03-24 11:26:52 -0500

    Initialized application

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-main>
+      <info-content />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import InfoContent from "@/components/InfoContent.vue";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: {InfoContent}
+};
 </script>
 
-<style>
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Hind:wght@300;400;500;600;700&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
+//noinspection ALL
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: 'Montserrat', serif;
+  font-size: 16px;
+  & .body-2 {
+    font-family: 'Hind', serif;
+  }
+  & .body-3 {
+    font-family: 'Hind', serif;
+    font-size: 14px;
+  }
+  & .body-4 {
+    font-weight: bold;
+    font-family: 'Hind', serif;
+    font-size: 14px;
+  }
+  & *[role=button] {
+    font-weight: 600;
+  }
+  & .heading-1 {
+    font-size: 32px;
+  }
+  & .heading-2 {
+    font-size: 24px;
+  }
+  & .heading-3 {
+    font-size: 24px;
+    font-weight: 600;
+  }
 }
 </style>
---
+++ src/components/InfoContent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="wrapper">
+    <div class="heading-2">Welcome to Study Together!</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InfoContent',
+}
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  max-width: 1000px;
+  margin: auto;
+}
+</style>
